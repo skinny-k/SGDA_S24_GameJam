8b026eff94a1861731c4f3781602c3257f5f45d4
Skinny <scott.krabbenhoft02@gmail.com>
2024-03-11 13:23:31 -0500

    Damage Callouts
    Fixed the damage callout system so that it should work better now. Had to update a physics query setting as well, but it hopefully shouldn't cause issues later.

--- Assets/Scripts/UI/WorldAnchoredUI.cs
+++ Assets/Scripts/UI/WorldAnchoredUI.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class WorldAnchoredUI : MonoBehaviour
 {
-    Vector3 _worldAnchor;
+    [SerializeField] float _xNoise = 0f;
+    [SerializeField] float _yNoise = 0f;
+    
+    [Header("Move Settings")]
+    [SerializeField] float _gravity = 0f;
+    [SerializeField] Vector3 _initialVelocity = Vector3.zero;
+    [SerializeField] float _velocityXNoise = 0f;
+    [SerializeField] float _velocityYNoise = 0f;
+    
     RectTransform _rt;
+    Vector3 _worldAnchor;
+    Vector3 _currentVelocity;
+    Vector3 _currentOffset = Vector3.zero;
 
     void Awake()
     {
         _rt = GetComponent<RectTransform>();
+        _currentOffset = new Vector3(Random.Range(-_xNoise, _xNoise), Random.Range(-_yNoise, _yNoise), 0);
+        _currentVelocity = _initialVelocity + new Vector3(Random.Range(-_velocityXNoise, _velocityXNoise), Random.Range(-_velocityYNoise, _velocityYNoise), 0);
     }
 
     void Update()
     {
-        // _rt.anchoredPosition3D = Camera.main.WorldToScreenPoint(_worldAnchor);
-        // Debug.Log(Camera.main.WorldToScreenPoint(_worldAnchor));
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(_worldAnchor);
+        screenPoint.x -= Screen.width / 2;
+        screenPoint.y -= Screen.height / 2;
+
+        if (_currentVelocity != Vector3.zero)
+        {
+            _currentOffset += _currentVelocity * Time.deltaTime;
+        }
+        if (_gravity != 0)
+        {
+            _currentVelocity += Vector3.down * _gravity * 9.81f * Time.deltaTime;
+        }
+
+        _rt.anchoredPosition3D = screenPoint + _currentOffset;
     }
 
     public void SetWorldAnchor(Vector3 newAnchor)
     {
         _worldAnchor = newAnchor;
-        _rt.anchoredPosition3D = Camera.main.WorldToScreenPoint(_worldAnchor);
-        Debug.Log(Camera.main.WorldToScreenPoint(_worldAnchor));
+
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(_worldAnchor);
+        screenPoint.x -= Screen.width / 2;
+        screenPoint.y -= Screen.height / 2;
+
+        _rt.anchoredPosition3D = screenPoint + _currentOffset;
     }
 }
