816c291fe0dd32a24985d6fc3d9034dc14796554
Eric WIlhoit <a10rec@myfastmail.com>
2024-03-13 00:39:35 -0500

    Titles
    Title UI element and FishingRod refactor.

--- Assets/Scripts/InteractObjects/FishingRod.cs
+++ Assets/Scripts/InteractObjects/FishingRod.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
-public class Fishing : MonoBehaviour
+public class FishingRod : InteractObject
 {
     [SerializeField] string _catchCalloutMessage = "Got a Bite!";
-    [SerializeField] string _failCalloutMessage = "Got a Bite!";
+    [SerializeField] string _failCalloutMessage = "Lost it...";
     [SerializeField] float _calloutOffsetY = 2f;
 
-    private StarterAssets.StarterAssetsInputs inputs;
+    //private StarterAssets.StarterAssetsInputs inputs;
     private PlayerInput playerInput;
     private bool _catchable = false;
     private bool _fishing = false;
+    private bool _title = false;
     private IEnumerator wait;
     private void Awake()
     {
-        inputs = FindObjectOfType<StarterAssets.StarterAssetsInputs>();
+        //inputs = FindObjectOfType<StarterAssets.StarterAssetsInputs>();
         playerInput = FindObjectOfType<PlayerInput>();
+    }
+
+    public override void Interact()
+    {
+        StartFishing();
     }
 
     public void StartFishing()
@@ -41,13 +46,7 @@ public class Fishing : MonoBehaviour
         }
         else
         {
-            _catchable = false;
-            _fishing = false;
-            StopCoroutine(wait);
-            //Cursor.lockState = CursorLockMode.Locked;
-            //inputs.cursorInputForLook = true;
-            playerInput.actions.FindAction("Move").Enable();
-            gameObject.GetComponent<InteractableObject>()._playerActions.Interacting = false;
+            EndFishing();
         }
     }
     public IEnumerator WaitForFish()
@@ -57,18 +56,27 @@ public class Fishing : MonoBehaviour
         MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), _catchCalloutMessage);
         yield return new WaitForSeconds(1);
         MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), _failCalloutMessage);
-        _catchable = false;
-        _fishing = false;
+        EndFishing();
     }
     private void CatchFish()
     {
+        MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), "Caught a Fish!");
+        if (!_title)
+        {
+            MasterUI.Instance.UpdateTitle(", Fisherman");
+            _title = true;
+        }
+        EndFishing();
+    }
+
+    private void EndFishing()
+    {
         _catchable = false;
+        _fishing = false;
         StopCoroutine(wait);
         //Cursor.lockState = CursorLockMode.Locked;
         //inputs.cursorInputForLook = true;
         playerInput.actions.FindAction("Move").Enable();
-        gameObject.GetComponent<InteractableObject>()._playerActions.Interacting = false;
-        _fishing = false;
-        MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), "Caught a Fish!");
+        //gameObject.GetComponent<InteractableObject>()._playerActions.Interacting = false;
     }
 }
