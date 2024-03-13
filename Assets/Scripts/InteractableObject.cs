4d3c3ee30beb81071428ef5a3c75ad79d2ec75fd
Eric WIlhoit <a10rec@myfastmail.com>
2024-03-12 23:30:03 -0500

    WIP Fishing
    Fishing rod prefab and script. If player presses interact nearby will start fishing and after random timer prompt player to catch fish.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -6,9 +6,12 @@ using UnityEngine.Events;
 public class InteractableObject : MonoBehaviour
 {
     public UnityEvent OnInteract;
+    [HideInInspector]
+    public PlayerActions _playerActions;
 
-    public void Interact()
+    public void Interact(PlayerActions playerActions)
     {
+        _playerActions = playerActions;
         OnInteract?.Invoke();
     }
 }
