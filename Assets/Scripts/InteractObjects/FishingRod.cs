9dbd3497aef815da2b68ea3ea280cf32f68efae2
Eric WIlhoit <a10rec@myfastmail.com>
2024-03-16 22:48:30 -0500

    Fish model

--- Assets/Scripts/InteractObjects/FishingRod.cs
+++ Assets/Scripts/InteractObjects/FishingRod.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+using UnityEngine.UI;
 
 public class FishingRod : InteractObject
 {
@@ -10,14 +12,16 @@ public class FishingRod : InteractObject
     [SerializeField] Color _calloutColor = Color.black;
     [SerializeField] float _calloutOffsetY = 2f;
 
-    //private StarterAssets.StarterAssetsInputs inputs;
+    private StarterAssets.StarterAssetsInputs inputs;
     private PlayerInput playerInput;
     private bool _catchable = false;
     private bool _fishing = false;
     private IEnumerator wait;
+    public GameObject Fish;
+    public Fish _fish;
     private void Awake()
     {
-        //inputs = FindObjectOfType<StarterAssets.StarterAssetsInputs>();
+        inputs = FindObjectOfType<StarterAssets.StarterAssetsInputs>();
         playerInput = FindObjectOfType<PlayerInput>();
     }
 
@@ -28,55 +32,69 @@ public class FishingRod : InteractObject
 
     public void StartFishing()
     {
-        if (_catchable)
+        if (!_fish._sitting)
         {
-            CatchFish();
-        }
-        else if(!_fishing)
-        {
-            //Replace with animation
-            MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), "Fishing...", _calloutColor);
+            if (_catchable)
+            {
+                CatchFish();
+            }
+            else if (!_fishing)
+            {
+                //Replace with animation
+                MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), "Fishing...", _calloutColor);
 
-            _fishing = true;
-            //Cursor.lockState = CursorLockMode.None;
-            //inputs.cursorInputForLook = false;
-            playerInput.actions.FindAction("Move").Disable();
-            wait = WaitForFish();
-            StartCoroutine(wait);         
-        }
-        else
-        {
-            EndFishing();
+                _fishing = true;
+                //Cursor.lockState = CursorLockMode.None;
+                //inputs.cursorInputForLook = false;
+                playerInput.actions.FindAction("Move").Disable();
+                wait = WaitForFish();
+                StartCoroutine(wait);
+            }
+            else
+            {
+                EndFishing();
+            }
         }
     }
     public IEnumerator WaitForFish()
     {
-        yield return new WaitForSeconds(Random.Range(3f, 10f));
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
         _catchable = true;
         MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), _catchCalloutMessage, _calloutColor);
         yield return new WaitForSeconds(1);
         MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), _failCalloutMessage, _calloutColor);
+        _catchable = false;
         EndFishing();
     }
     private void CatchFish()
     {
+        inputs.cursorInputForLook = false;
         MasterUI.Instance.DamageCallout(transform.position + new Vector3(0, _calloutOffsetY, 0), "Caught a Fish!", _calloutColor);
         if (FindObjectOfType<PlayerInfo>().GetQuestStatus(Quest.Fish) == QuestStatus.NotStarted)
         {
             FindObjectOfType<PlayerInfo>().UpdateQuestStatus(Quest.Fish, QuestStatus.Completed);
             MasterUI.Instance.UpdateTitle(", Fisherman");
         }
+        Fish.SetActive(true);
         EndFishing();
     }
 
     private void EndFishing()
     {
+        if (_catchable && Fish != null)
+        {
+            _fish._player.GetComponent<PlayerBase>().Actions.OnInteract += Fish.transform.parent.GetComponent<Fish>().Pickup;
+            _fish.Pickup();
+        }
         _catchable = false;
         _fishing = false;
+       
         StopCoroutine(wait);
         //Cursor.lockState = CursorLockMode.Locked;
-        //inputs.cursorInputForLook = true;
         playerInput.actions.FindAction("Move").Enable();
+        _playerInRange.Actions.OnInteract -= Interact;
+        _tip.gameObject.GetComponent<Image>().enabled = false;
+        _tip.gameObject.transform.GetChild(0).gameObject.SetActive(false);
         //gameObject.GetComponent<InteractableObject>()._playerActions.Interacting = false;
     }
 }
