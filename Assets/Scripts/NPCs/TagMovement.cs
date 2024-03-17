65061f5322be43e53281c124f80f3a73f3aa9443
Eric WIlhoit <a10rec@myfastmail.com>
2024-03-16 19:11:53 -0500

    Tag Game

--- Assets/Scripts/NPCs/TagMovement.cs
+++ Assets/Scripts/NPCs/TagMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TagMovement : NPCMovement
 {
     PlayerBase _playingTag = null;
     TagBody _tb;
 
+    public GameObject DialogueNPC;
     public TagBody TB => _tb;
 
     public void SetTagGame(TagGame game)
@@ -42,13 +44,18 @@ public class TagMovement : NPCMovement
 
     public void StartTag(PlayerBase player)
     {
+        Destroy(gameObject.GetComponent<Dialogue>());
         _playingTag = player;
         _tb.gameObject.SetActive(true);
     }
 
     public void EndTag()
     {
+        DialogueNPC.transform.position = gameObject.transform.position;
+        DialogueNPC.transform.rotation = gameObject.transform.rotation;
+        DialogueNPC.SetActive(true);
         _agent.ResetPath();
         _playingTag = null;
+        gameObject.SetActive(false);
     }
 }
